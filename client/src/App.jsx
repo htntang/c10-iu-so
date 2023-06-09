e8f53dec7b86fc78f28df2124c2299826237b062
htntang <htntang@gmail.com>
2023-06-09 14:24:15 -0600

    added pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,31 @@
 import { useState } from 'react'
 import './App.css'
+import { Link, Route, Routes } from 'react-router-dom'
+import Layout from './Layout'
+import HomePage from './Pages/HomePage'
+import LoginPage from './Pages/LoginPage'
+import RegistrationPage from './Pages/RegistrationPage'
+import Blog from './Pages/Blog'
+import Team from './Pages/Team'
+import Mentorship from './Pages/Mentorship'
+import ScholarshipDatabase from './Pages/ScholarshipDatabase'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <header>
-          StriveOpp
-        </header>
-        <footer></footer>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route index="/login" element={<LoginPage />} />
+          <Route index="/register" element={<RegistrationPage />} />
+          <Route index="/blog" element={<Blog />} />
+          <Route index="/team" element={<Team />}/>
+          <Route index="/mentorship" element={<Mentorship />} />
+          <Route index="/database" element={<ScholarshipDatabase />} />
+        </Route>
+      </Routes>
     </>
   )
 }
---
+++ client/src/Pages/ScholarshipDatabase.jsx
@@ -0,0 +1,5 @@
+export default function ScholarshipDatabase(){
+    return(
+        <h1>Scholarship Database</h1>
+    )
+}
