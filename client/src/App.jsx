bb25a240bdedc5745421697032b1d6a481419ccb
htntang <htntang@gmail.com>
2023-06-09 14:09:43 -0600

    began new frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <header>
+          StriveOpp
+        </header>
+        <footer></footer>
+    </>
+  )
+}
+
+export default App
