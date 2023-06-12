2273c97a234f2fdedc935dce09e66fa067254cb9
htntang <htntang@gmail.com>
2023-06-11 21:30:52 -0600

    edited and formatted the team bio page

--- client/src/Pages/Team.jsx
+++ client/src/Pages/Team.jsx
@@ -1,26 +1,59 @@
+import React from "react"
+import DawnBiographyCard from "../components/Bio-Cards/Dawn-Bio"
+import DolapoBiographyCard from "../components/Bio-Cards/Dolapo-Bio"
+import ElvinBiographyCard from "../components/Bio-Cards/Elvin-Bio"
+import HanBiographyCard from "../components/Bio-Cards/Han-Bio"
+import NicolasBiographyCard from "../components/Bio-Cards/Nicolas-Bio"
+import RenataBiographyCard from "../components/Bio-Cards/Renata-Bio"
+
 export default function Team(){
     return(
-        <>
-        {/*Han Tang*/}
-        <div class="card">
-            <div class="card_inner">
-                <div class="card_face card__face--front">
-                    <h2>Dev Card</h2>
-                </div>
-                <div class="card__face card__face--back">
-                    <div class="card__content">
-                        <div class="card__header">
-                            <img src="" alt="" class="pp"/>
-                            <h2>Han Tang</h2>
-                        </div>
-                        <div class="card__body">
-                            <h3>Full Stack Developer</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi alias odit, nam consequuntur debitis voluptatibus, labore esse placeat vero, sed adipisci accusantium deleniti minus reprehenderit suscipit laudantium. Distinctio, vero? Iste?</p>
-                        </div>
-                    </div>
-                </div>
+        <>    
+        <div className="team">
+
+        <h1><i>Meet the Team</i></h1>
+
+        <div className="row">
+
+            <div className="column">
+                <DolapoBiographyCard />
             </div>
+
+            <div className="column">
+                <DawnBiographyCard />
+            </div>
+            
         </div>
+
+        <div className="row">
+
+            <div className="column">
+                <ElvinBiographyCard />
+            </div>
+
+            <div className="column">
+                <NicolasBiographyCard />
+            </div>
+            
+            </div>
+
+        <div className="row">
+
+            <div className="column">
+                <RenataBiographyCard />
+            </div>
+
+            <div className="column">
+                <HanBiographyCard />
+            </div>
+
+            
+        </div>
+        </div>
+
+                
+                
+
         </>
     )
 }
